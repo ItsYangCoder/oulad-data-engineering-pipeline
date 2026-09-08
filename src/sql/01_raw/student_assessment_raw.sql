e85b5db1e9bcd7a6bb9a3c63e36b9a8351305db7
ItsYangCoder <rhea.alum@ftwfoundation.org>
2026-09-08 18:06:24 +0800

    Fix typo in student_assessment_raw table name

--- src/sql/01_raw/student_assessment_raw.sql
+++ src/sql/01_raw/student_assessment_raw.sql
@@ -1,6 +1,6 @@
 -- Suggested branch: feature/bronze-ingestion
 
-CREATE TABLE IF NOT EXISTS open_university.oulad_bronze.student_assesments_raw
+CREATE TABLE IF NOT EXISTS open_university.oulad_bronze.student_assesment_raw
 USING DELTA
 AS 
 SELECT *, 
